8cc452a76add971c9ffb5bc0759e779e0e066715
Luiz Claudio <luizclaudiomaegima@gmail.com>
2024-05-16 20:54:57 -0300

    criando a tela de login e tela  inicial

--- escalas/views.py
+++ escalas/views.py
@@ -2,23 +2,30 @@ from django.shortcuts import render
 from django.views.generic import ListView,CreateView,UpdateView,DeleteView
 from .models import funcionario
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+
 
 def home (request):
     return render(request,"pagina_inicial.html")
 
+def tela_login(request):
+    return render(request,"tela_inicial.html")
 
 class FuncionarioListView(ListView):
   model = funcionario
+
 
 class FuncionarioCreateView(CreateView):
     model = funcionario
     fields =['Nome','Sobrenome','Registro_Empresa','Escala_de_trabalho']
     success_url = reverse_lazy("lista")
 
+
 class FuncionarioUpdateView(UpdateView):
     model = funcionario
     fields =['Nome','Sobrenome','Registro_Empresa','Escala_de_trabalho']
     success_url = reverse_lazy("lista")
+
 
 class FuncionarioDeleteView(DeleteView):
     model = funcionario
